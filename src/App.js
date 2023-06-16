26ab05ebd8c1d2704d0050900a384566f9191547
felipekmu <132099333+felipekmu@users.noreply.github.com>
2023-06-16 17:33:11 -0300

    Aula do Dia 16 de Junho

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [ pokemons, alteraPokemons ] = React.useState ( [] );
+  const [ txtPokemon, alteraTxtPokemon ] = React.useState("");
+
+function buscaPokemon(){
+  axios.get("https://pokeapi.co/api/v2/pokemon/"+txtPokemon)
+    .then( Response => {
+      console.log("Requisição bem sucedida!");
+      alteraPokemons  (Response.data.results);
+    })
+    .catch( Response => {
+      console.log("Deu ruim na requisição");
+      console.log(Response);
+    })
+}
+
+  function buscaTodosPokemons(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=50")
+    .then( Response => {
+      console.log("Requisição bem sucedida!");
+      alteraPokemons  (Response.data.results);
+    })
+    .catch( Response => {
+      console.log("Deu ruim na requisição");
+      console.log(Response);
+    })
+  }
+ // if ( pokemons.length == 0 ){
+   // buscaTodosPokemons();
+// }  
+
+React.useEffect( ()=>{
+  buscaTodosPokemons();
+}, [] );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Felipe PokéDex </h1>
+      <p> Conheça os Pokémons mais famosos </p>
+
+      <input onChange={ (e)=> alteraTxtPokemon( e.target.value ) } placeholder='Digite o nome de um Pokémon'/>
+      <button onClick={ ()=> buscaPokemon() }>Buscar</button>
+      
+      {
+        pokemons.map( (pokemon,index) =>
+          <div>
+            <p> {pokemon.name} </p>
+            <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+(index+1)+".gif"}/>
+            </div>
+          )
+      }
     </div>
   );
 }
